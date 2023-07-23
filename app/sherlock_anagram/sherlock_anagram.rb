b2ba8b1118f899bd89980d966cd4c85c8b48f171
Carl Tablante <carlwilliam.tablante@gmail.com>
2023-07-23 18:34:14 +1000

    merged sherlock-anagram

---
+++ app/sherlock_anagram/sherlock_anagram.rb
@@ -0,0 +1,35 @@
+class SherlockAnagram
+  def self.run(s)
+    new.sherlock_anagram(s)
+  end
+
+  def substrings(string)
+    string_array = string.chars
+    container = Hash.new(0)
+
+    # abba -> [a, b, b, a]
+    string_array.each.with_index do |s, i|
+      substring = string_array[i..-1] || []
+
+      substring.size.times do |i2|
+        end_index = i2 + i
+        key = string_array[i..end_index]&.sort&.join
+
+        container[key] += 1
+      end
+    end
+
+    container
+  end
+
+  def sherlock_anagram(str)
+    # get all substrings
+    substrings_list = substrings(str)
+
+    # Formula to get number of copmarisons in a set
+    # n * (n - 1) / 2
+    substrings_list.values.reduce(0) do |memo, val|
+      memo + (val * (val - 1) / 2)
+    end
+  end
+end
--- spec/sherlock_anagram_spec.rb
+++ spec/sherlock_anagram_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
-require_relative '../app/sherlock_anagram/anagram'
+require_relative '../app/sherlock_anagram/sherlock_anagram'
 
 describe SherlockAnagram do
   describe "#run" do
-    subject { described_class.new(input).run }
+    subject { described_class.run(input) }
 
     context "when input 'ifailuhkqq'" do
       let(:input) { 'ifailuhkqq' }
 
-      it { is_expected.to eq 2 }
+      it { is_expected.to eq 3 }
     end
 
     context "when input 'abba'" do
       let(:input) { 'abba' }
 
-      it { is_expected.to eq 2 }
+      it { is_expected.to eq 4 }
     end
 
     context "when input 'abcd'" do
@@ -34,5 +34,33 @@ describe SherlockAnagram do
 
       it { is_expected.to eq 10 }
     end
+
+    context "when input 'mom'" do
+      let(:input) { 'mom' }
+
+      it { is_expected.to eq 2 }
+    end
+
+    context 'when input is ifailuhkqqhucpoltgtyovarjsnrbfpvmupwjjjfiwwhrlkpekxxnebfrwibylcvkfealgonjkzwlyfhhkefuvgndgdnbelgruel' do
+      let(:input) { 'ifailuhkqqhucpoltgtyovarjsnrbfpvmupwjjjfiwwhrlkpekxxnebfrwibylcvkfealgonjkzwlyfhhkefuvgndgdnbelgruel'}
+
+      it { is_expected.to eq 399}
+    end
+
+    context 'when input is gffryqktmwocejbxfidpjfgrrkpowoxwggxaknmltjcpazgtnakcfcogzatyskqjyorcftwxjrtgayvllutrjxpbzggjxbmxpnde' do
+      let(:input) { 'gffryqktmwocejbxfidpjfgrrkpowoxwggxaknmltjcpazgtnakcfcogzatyskqjyorcftwxjrtgayvllutrjxpbzggjxbmxpnde' }
+
+      it { is_expected.to eq 471 }
+    end
   end
+
+  # describe '#substrings' do
+  #   subject { described_class.new.substrings(input) }
+
+  #   context 'when input is abba' do
+  #     let(:input) { 'abba' }
+
+  #     it { is_expected.to eq ['a','ab','abb','abba','b','bb','bba','b','ba','a']  }
+  #   end
+  # end
 end
